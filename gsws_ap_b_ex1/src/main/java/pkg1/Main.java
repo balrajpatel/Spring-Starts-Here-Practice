6452e5286a29aa6394f2e6cca136e08ee9215db3
Balraj Patel <balrajpatel06@gmail.com>
2026-01-09 00:51:40 +0530

    Change java version to 17.

--- gsws_ap_b_ex1/src/main/java/pkg1/Main.java
+++ gsws_ap_b_ex1/src/main/java/pkg1/Main.java
@@ -1,7 +1,10 @@
 package pkg1;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext();
         Parrot p = new Parrot();
     }
 }

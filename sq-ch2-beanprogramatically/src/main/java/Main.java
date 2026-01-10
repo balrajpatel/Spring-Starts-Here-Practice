6d8c975bc25eadae6758ba5ae779f49d7793701c
Balraj Patel <balrajpatel06@gmail.com>
2026-01-10 12:26:31 +0530

    Programmatically add bean to spring context.

---
+++ sq-ch2-beanprogramatically/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import Main.Parrot;
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot parrot = new Parrot();
+        parrot.setName("Kiki");
+        Supplier<Parrot> supplier = () -> parrot;
+        //defined Supplier to return parrot instance which we want to add in spring context
+        context.registerBean("parrot1",Parrot.class,supplier);
+        //4th param can omit, because its a var args, it helps to configure this  instance like making it primary;
+
+        Parrot parrot2 = context.getBean(Parrot.class);
+                System.out.println(parrot2.getName());
+    }
+}

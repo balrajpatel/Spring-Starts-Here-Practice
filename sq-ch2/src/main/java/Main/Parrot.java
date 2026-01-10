74bc62a106052e88ec63bc46d37d79e7a2461a5d
Balraj Patel <balrajpatel06@gmail.com>
2026-01-10 12:06:56 +0530

    Add @PostConstruct annotation.

--- sq-ch2/src/main/java/Main/Parrot.java
+++ sq-ch2/src/main/java/Main/Parrot.java
@@ -1,7 +1,11 @@
 package Main;    //this is used as basePackage in ComponentScan
 
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
 @Component     //instruct to Which classes to add an instance to its context
+// it instructs spring to creat an object of that class (ctor is called) and add in its context
 public class Parrot {
     private String name;
     public String getName() {
@@ -9,5 +13,10 @@ public class Parrot {
     }
     public void setName(String name) {
         this.name = name;
+    }
+    @PostConstruct  // it instructs spring to execute it just after the ctor is called (ctor used to create obj instance)
+    //used to execute some instructions just after obj creation
+    public void init() {
+        this.name = "Kiki";
     }
 }

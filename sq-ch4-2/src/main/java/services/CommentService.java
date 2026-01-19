dba2fcd1dece96f5c55646ff819ad0f19984c0d9
Balraj Patel <balrajpatel06@gmail.com>
2026-01-19 10:33:02 +0530

    Implement CommentService Use Case using stereotype annotation (Component).

---
+++ sq-ch4-2/src/main/java/services/CommentService.java
@@ -0,0 +1,20 @@
+package services;
+
+import model.Comment;
+import org.springframework.stereotype.Component;
+import proxies.CommentNotificationProxy;
+import repositories.CommentRepository;
+@Component
+public class CommentService {
+    private final CommentNotificationProxy commentNotificationProxy;
+    private final CommentRepository commentRepository;
+    public CommentService(CommentNotificationProxy commentNotificationProxy, CommentRepository commentRepository) {
+        this.commentNotificationProxy = commentNotificationProxy;
+        this.commentRepository = commentRepository;
+    }
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+
+}

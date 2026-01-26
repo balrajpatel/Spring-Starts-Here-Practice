8d1b6214c23f93dda7c49f7adb02ac3909cf87a9
Balraj Patel <balrajpatel06@gmail.com>
2026-01-26 21:59:09 +0530

    Implement aspect logic with altering intercepted method parameters and returned value.

---
+++ sq-ch6-4/src/main/java/aspects/LoggingAspect.java
@@ -0,0 +1,33 @@
+package aspects;
+
+import model.Comment;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+@Component
+@Aspect
+public class LoggingAspect {
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+    @Around("execution(* services.*.*(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        String methodName = joinPoint.getSignature().getName();
+        Comment comment = new Comment();
+        comment.setText("Demo comment from Aspect");
+        comment.setAuthor("Anderson Peters");
+        Object[] newArgs = {comment};
+        logger.info("Method: "+methodName + " with parameters: "+ Arrays.asList(args) + " will be executed");
+
+        Object result = joinPoint.proceed(newArgs);
+        logger.info("Method executed and received result: "+result);
+        return "FAILED";
+
+
+        // joinPoint()  required array of Object
+    }
+}

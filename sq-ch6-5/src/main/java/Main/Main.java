07716ea3e16fae114fabe185ea9e01d4a0627c93
Balraj Patel <balrajpatel06@gmail.com>
2026-01-26 22:34:41 +0530

    Implement custom annotation to weave aspect to custom annotated methods.

---
+++ sq-ch6-5/src/main/java/Main/Main.java
@@ -0,0 +1,27 @@
+package Main;
+
+import config.ProjectConfig;
+import model.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+         Logger logger = Logger.getLogger(Main.class.getName());
+         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+         Comment comment = new Comment();
+         comment.setText("Demo comment");
+         comment.setAuthor("John Doe");
+         CommentService commentService = context.getBean(CommentService.class);
+         /*
+         String result = commentService.publishComment(comment);
+         logger.info(result);
+         since not an intercepted aspect method
+          */
+        String result = commentService.deleteComment(comment);
+        logger.info(result);
+
+    }
+}
